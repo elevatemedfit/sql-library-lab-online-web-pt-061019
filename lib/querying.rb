b6e9e204b427025dcbc59fd9dd27926062c34cf7
Chris Junker <elevatemedfit@gmail.com>
2019-08-21 18:31:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,10 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species
+  "SELECT characters.species COUNT(*)
   FROM characters
-  GROUP BY characters.species
-  ORDER BY COUNT(*) DESC
+  GROUP BY characters.species DESC
   LIMIT 1;"
 end
 
