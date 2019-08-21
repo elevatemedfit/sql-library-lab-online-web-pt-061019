89394b5463adfd9c4232edcdad7b2cf33b3fba92
Chris Junker <elevatemedfit@gmail.com>
2019-08-21 18:54:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,12 +28,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.author_id, characters.author._id
-  FROM characters
-  INNER JOIN series
-  ON characters.author_id = series.author_id
-  ORDER BY characters.author_id DESC
-  LIMIT 1;"
+  "SELECT series.title
+  FROM series
+  JOIN books
+  ON books.series_id = series.id
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON character_books.character.id = character.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(*)
+  DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
