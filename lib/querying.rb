86d5744d0307b3aae4fb0b034cbdb7505ce8e563
Chris Junker <elevatemedfit@gmail.com>
2019-08-21 18:39:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,6 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.subgenre_id
   FROM series
-  INNER JOIN series
-  ON authors.id = sweries.subgenre_id
   ORDER BY authors ASC;"
 end
 
